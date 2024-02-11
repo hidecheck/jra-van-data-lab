d8239f9db4348e4a450aa6eb7a1ff3b25da27658
hidecheck <hidecheck@gmail.com>
2024-02-11 17:58:48 +0900

    update favorite_service_client

--- examples/favorite_service_client.py
+++ examples/favorite_service_client.py
@@ -4,6 +4,33 @@ from repository.race_repository import RaceRepository
 from service.favorite_service import FavoriteService
 
 
+def get_favorite_rate():
+    conditions_string = None
+
+    # # 障害レースなしの日
+    conditions = {
+        "kaisai_nen": "2019",
+        "kaisai_tsukihi": "0126",
+        "keibajo_code": "05",
+    }
+    # # 障害レースありの日
+    # conditions = {
+    #     # "kaisai_nen": "2020",
+    #     # "kaisai_tsukihi": "1226",
+    #     "keibajo_code": "06",
+    # }
+
+    race_repository = RaceRepository()
+    entry_horses_repository = EntryHorsesRepository()
+    payoff_repository = PayoffRepository()
+
+    service = FavoriteService(race_repository=race_repository, entry_horses_repository=entry_horses_repository,
+                              payoff_repository=payoff_repository, conditions=conditions,
+                              conditions_string=conditions_string, favorite=1)
+
+    print(f"{service.favorite}人気: {service.statistics}")
+
+
 def get_top_10_years_rate():
     """
     10 年間の東京競馬場 1 番人気のデータ集計
@@ -21,11 +48,12 @@ def get_top_10_years_rate():
                               payoff_repository=payoff_repository, conditions=conditions,
                               conditions_string=conditions_string, favorite=1)
 
-    print(service.statictics)
+    print(f"{service.favorite}人気: {service.statistics}")
 
 
 def main():
-    get_top_10_years_rate()
+    get_favorite_rate()
+    # get_top_10_years_rate()
 
 
 if __name__ == '__main__':
