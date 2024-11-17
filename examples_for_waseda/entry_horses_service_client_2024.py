b8406472ac7e7fd46a10b1e016701d06c9bceee2
hidecheck <hidecheck@gmail.com>
2024-11-17 18:08:17 +0900

    fix #23 EntryHorsesServiceにレースIDを追加する

--- examples_for_waseda/entry_horses_service_client_2024.py
+++ examples_for_waseda/entry_horses_service_client_2024.py
@@ -1,13 +1,8 @@
 import pandas as pd
 
 import utils
-from const.table_columns import jvd_se
-from const.table_columns.jvd_ra import CUSTOM_COL_RACE_ID
-from const.table_columns.jvd_se import CUSTOM_COL_WIN_BET, CUSTOM_COL_WIN_FAVORITE, CUSTOM_COL_GENDER, \
-    CUSTOM_COL_WEIGHT_DIFFERENCE
 from repository.entry_horses_repository import EntryHorsesRepository
 from service.entry_horses_service import EntryHorsesService
-from utils import output
 
 
 def create_csv(conditions, file_name):
@@ -15,11 +10,27 @@ def create_csv(conditions, file_name):
     # service = EntryHorsesService(conditions=conditions, conditions_string=conditions_string)
     service = EntryHorsesService(conditions=conditions)
 
-    df = service.entry_horses.loc[:, jvd_se.MINIMUM_COLUMNS]
-    output.show_one_line(df, False)
-    print(df)
-    df.to_csv(file_name, index=False)
+    df = service.entry_horses
+    utils.output.show_one_line(df)
 
+    out_df = pd.DataFrame()
+    out_df["race_bango"] = df["race_bango"]
+    out_df["umaban"] = df["umaban"]
+    out_df["bamei"] = df["bamei"].apply(lambda x: x.strip())
+    # out_df["bamei"] = df["bamei"]
+    out_df["bataiju"] = df["bataiju"]
+    out_df["zogen_sa"] = df["zogen_sa"]
+    out_df["kohan_3f"] = df["kohan_3f"]
+    # out_df["tansho_ninkijun"] = df["tansho_ninkijun"]
+    out_df["tansho_odds"] = df["tansho_odds"]
+    out_df["kakutei_chakujun"] = df["kakutei_chakujun"]
+    out_df["time_sa"] = df["time_sa"]
+
+    # for index, row in service.entry_horses.iterrows():
+    #     print(row["race_bango"], row["umaban"], row["bamei"], row["bataiju"], row["tansho_ninkijun"],
+    #           row["tansho_odds"])
+    print(out_df)
+    out_df.to_csv(file_name, index=False)
 
 def group_by_bataiju(file_name):
     df = pd.read_csv(file_name)
@@ -64,10 +75,10 @@ def main():
         "race_bango": "11"
     }
 
+    file_name = "entry_horses_service_client.csv"
     file_name = "2019-12-22_nakayama_11R.csv"
     create_csv(conditions, file_name)
     # group_by_bataiju(file_name)
-
 
 if __name__ == '__main__':
     main()
